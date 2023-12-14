3f90543109bd0d6ba60ef3442a2e0c6d28f58d26
rayozin <142932535+rayozin@users.noreply.github.com>
2023-12-14 15:44:04 -0300

    drytst

---
+++ main.js
@@ -0,0 +1,22 @@
+let nome = "Ray ";
+let sobrenome = "Vieira";
+
+let nomecompleto = nome.concat(sobrenome);
+console.log(nomecompleto);
+
+let quantcar = nomecompleto.length;
+console.log(quantcar);
+
+let irmao = nomecompleto.replace("Ray", "Rafael");
+console.log(irmao);
+
+let a = Math.floor(Math.random()*100);
+console.log(a);
+
+let b = Math.floor(Math.random()*100);
+console.log(b);
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
